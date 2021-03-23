66f6456c4396896636cdad47e531026d96af6e0f
NoahG3493 <81201852+NoahG3493@users.noreply.github.com>
2021-03-22 22:50:36 -0400

    Add files via upload

---
+++ airline.py
@@ -0,0 +1,5 @@
+# Noah Gebremariam & Genesis Clarke
+# CS330-02
+# This program will create an airline
+# that will determine the seating of different
+# travelers such as families, business, and tourists
