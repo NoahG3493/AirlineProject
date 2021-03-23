9ce64c1d63d1830c19b1666dc3ac337ae798ffe6
NoahG3493 <81201852+NoahG3493@users.noreply.github.com>
2021-03-23 11:04:09 -0400

    Delete airline.py

--- airline.py
+++
@@ -1,5 +0,0 @@
-# Noah Gebremariam & Genesis Clarke
-# CS330-02
-# This program will create an airline
-# that will determine the seating of different
-# travelers such as families, business, and tourists
