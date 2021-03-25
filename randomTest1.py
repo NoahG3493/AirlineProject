20f3b9444205684d55169d4cec8cb425230395c7
NoahG3493 <ngebremariam2@capital,edu>
2021-03-25 11:58:32 -0400

    add test

---
+++ randomTest1.py
@@ -0,0 +1,9 @@
+# Noah Gebremariam & Genesis Clarke
+# CS330-02
+
+def main():
+    # This allows user to input a number
+    # and it will print the number
+    x = int(input("Enter a number: "))
+    print(x)
+main()
